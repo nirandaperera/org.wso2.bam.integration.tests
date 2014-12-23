a7708ed4e7eeb03c0156b1c0f7dff4b8b833307a
niranda <niranda.perera@gmail.com>
2014-12-23 11:48:44 +0530

    final changes to the java test cases

--- src/test/java/org/wso2/bam/integration/tests/toolbox/ToolboxDeploymentValidationTestCase.java
+++ src/test/java/org/wso2/bam/integration/tests/toolbox/ToolboxDeploymentValidationTestCase.java
@@ -1,21 +1,22 @@
-package org.wso2.bam.integration.tests.toolbox;
-
-/**
- * Copyright (c) 2009, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+/*
+ * Copyright (c) 2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
+package org.wso2.bam.integration.tests.toolbox;
 
 import org.apache.axis2.AxisFault;
 import org.apache.axis2.client.Options;
@@ -47,11 +48,11 @@ import java.net.URL;
 import static org.testng.Assert.assertTrue;
 
 /**
-* This test case validates the following use case.
-* Once a toolbox is deployed, the analytic scripts are being saved in the registry.
-* It may be needed to change these analytic scripts time to time and the most convenient way to achieve this is, replacing the toolbox in the deployment folder of BAM.
-* This test validates if the existing scripts in the registry are being correctly replaced by the new scripts.
-*/
+ * This test case validates the following use case.
+ * Once a toolbox is deployed, the analytic scripts are being saved in the registry.
+ * It may be needed to change these analytic scripts time to time and the most convenient way to achieve this is, replacing the toolbox in the deployment folder of BAM.
+ * This test validates if the existing scripts in the registry are being correctly replaced by the new scripts.
+ */
 public class ToolboxDeploymentValidationTestCase {
     private static final Log log = LogFactory.getLog(ToolboxDeploymentValidationTestCase.class);
 
@@ -59,11 +60,9 @@ public class ToolboxDeploymentValidationTestCase {
     private static final String REGISTRY_SERVICE = "/services/ResourceAdminService";
     private static final String SERVICE_STAT_TBOX_NAME = "Service_Statistics_Monitoring.tbox";
     private static final String HIVE_SCRIPT_LOCATION = "/_system/config/repository/hive/scripts/";
-    private static final String SERVICE_STAT_TBOX = "/samples/toolboxes/Service_Statistics_Monitoring.tbox";
+    private static final String SERVICE_STAT_TBOX_PATH = "/samples/toolboxes/Service_Statistics_Monitoring.tbox";
     private static final String HOST_NAME = "localhost";
-
-    private static final URL SERVICE_STAT_TBOX_DUMMY = ToolboxDeploymentValidationTestCase.class.getClassLoader()
-            .getResource(SERVICE_STAT_TBOX_NAME);
+    private static final String AUTHENTICATION_SERVICE = "/services/AuthenticationAdmin";
 
     private int portOffset = 3;
     private String carbonZip;
@@ -71,85 +70,57 @@ public class ToolboxDeploymentValidationTestCase {
     private String serviceStatToolBoxDestination;
     private String initialHiveScriptName;
     private String initialScript;
-    private String replacedScript;
     private ResourceAdminServiceStub registryStub;
     private String sessionCookie;
 
     private ServerUtils serverUtils = new ServerUtils();
+    private static final URL SERVICE_STAT_TBOX_DUMMY = ToolboxDeploymentValidationTestCase.class.getClassLoader()
+            .getResource(SERVICE_STAT_TBOX_NAME);
 
-    @BeforeClass(groups = {"wso2.bam"}, timeOut = 360000)
+    @BeforeClass(groups = {"wso2.bam"}, timeOut = 180000)
     public void init() throws Exception {
-
-
-        log.info("Unpacking BAM zip file");
+        log.info("Starting the Offline Toolbox Deployment Validation TestCase ...");
         carbonHome = unpackCarbonZip();
-        log.info("Carbon home: " + carbonHome);
 
-        log.info("Copying Service Stat Toolbox");
-        String serviceStatToolBoxSource = carbonHome + SERVICE_STAT_TBOX;
-        log.info("SERVICE_STAT_TBOX_NAME source: " + serviceStatToolBoxSource);
+        //Copy the toolbox to the BAM
+        String serviceStatToolBoxSource = carbonHome + SERVICE_STAT_TBOX_PATH;
         serviceStatToolBoxDestination = carbonHome + BAM_TBOX_DEPLOYMENT_DIR + SERVICE_STAT_TBOX_NAME;
-        log.info("SERVICE_STAT_TBOX_NAME destination: " + serviceStatToolBoxDestination);
-        try {
-            copyFile(new File(serviceStatToolBoxSource), new File(serviceStatToolBoxDestination));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        FileUtils.copyFile(new File(serviceStatToolBoxSource), new File(serviceStatToolBoxDestination));
 
-        log.info("Starting the Carbon server");
         startServer();
-
-        log.info("Logging into Carbon server");
-
-        String loggedInSessionCookie = login();// loginLogoutUtil.login();
-
-        log.info("Logged in session cookie : " + loggedInSessionCookie);
-
+        String loggedInSessionCookie = login();
         ConfigurationContext configContext = ConfigurationContextFactory.
                 createConfigurationContextFromFileSystem(null);
         initializedRegistryStub(loggedInSessionCookie, configContext);
     }
 
-
     @Test(groups = {"wso2.bam"})
     public void readingInitialHiveScripts() throws Exception {
-        log.info("Reading initial Hive scripts");
-
         CollectionContentBean collection = registryStub
                 .getCollectionContent(HIVE_SCRIPT_LOCATION);
-
         initialHiveScriptName = collection.getChildPaths()[0];
-        log.info("Initial Hive script name : " + initialHiveScriptName);
-
         initialScript = registryStub
                 .getTextContent(initialHiveScriptName).trim();
-
-        String[] initialQueries = initialScript.split(";");
-        logQueries(initialQueries);
     }
 
     @Test(groups = {"wso2.bam"}, dependsOnMethods = {"readingInitialHiveScripts"}, timeOut = 180000)
     public void temporarilyLogoutAndStopServer() throws Exception {
-        log.info("Temporarily logging out and stopping the Carbon server");
-        logoutAndStopServer();
+        ClientConnectionUtil.waitForPort(Integer.parseInt(FrameworkSettings.HTTPS_PORT));
+        logout();
+        stopServer();
+
+        //Copying the new toolbox
+        File destFile = new File(serviceStatToolBoxDestination);
+        if (destFile.exists()) {
+            destFile.delete();
+        }
+        FileUtils.copyFile(new File(SERVICE_STAT_TBOX_DUMMY.getFile()), destFile);
     }
 
-    @Test(groups = {"wso2.bam"}, dependsOnMethods = {"temporarilyLogoutAndStopServer"})
-    public void replaceToolbox() throws Exception {
-        log.info("Replacing toolbox from another toolbox!");
-        copyFile(new File(SERVICE_STAT_TBOX_DUMMY.getFile()), new File(serviceStatToolBoxDestination));
-    }
-
-    @Test(groups = {"wso2.bam"}, dependsOnMethods = {"replaceToolbox"}, timeOut = 180000)
+    @Test(groups = {"wso2.bam"}, dependsOnMethods = {"temporarilyLogoutAndStopServer"}, timeOut = 180000)
     public void restartServer() throws Exception {
-        log.info("Restarting the Carbon server with the replaced toolbox");
         startServer();
-
-        log.info("Logging into Carbon server");
-
         String loggedInSessionCookie = login();
-        log.info("Logged in session cookie : " + loggedInSessionCookie);
-
         ConfigurationContext configContext = ConfigurationContextFactory.
                 createConfigurationContextFromFileSystem(null);
         initializedRegistryStub(loggedInSessionCookie, configContext);
@@ -160,27 +131,15 @@ public class ToolboxDeploymentValidationTestCase {
         boolean nameReplaced = false;
         boolean contentReplaced = false;
 
-        log.info("Reading Hive scripts after server restart");
+        //Reading Hive scripts after server restart
         CollectionContentBean collection = registryStub
                 .getCollectionContent(HIVE_SCRIPT_LOCATION);
-
         String replacedHiveScriptName = collection.getChildPaths()[0];
-
-        log.info("Replaced Hive script name : " + replacedHiveScriptName);
-
-        replacedScript = registryStub
-                .getTextContent(replacedHiveScriptName).trim();
-
-        String[] replacedHiveQueries = replacedScript.split(";");
-        logQueries(replacedHiveQueries);
-
+        String replacedScript = registryStub.getTextContent(replacedHiveScriptName).trim();
         if (!replacedHiveScriptName.equals(initialHiveScriptName)) {
             nameReplaced = true;
-            log.info("Script name replaced!");
-
             if (!replacedScript.equals(initialScript)) {
                 contentReplaced = true;
-                log.info("Script replaced!");
             }
         }
 
@@ -188,28 +147,11 @@ public class ToolboxDeploymentValidationTestCase {
         assertTrue(contentReplaced, "Script content is the same!");
     }
 
-
     @AfterClass(groups = {"wso2.bam"}, timeOut = 180000)
     public void logoutAndStopServer() throws Exception {
-        log.info("Logging out from Carbon server");
         ClientConnectionUtil.waitForPort(Integer.parseInt(FrameworkSettings.HTTPS_PORT));
         logout();
-
-        log.info("Stopping the server");
         stopServer();
-    }
-
-
-    protected void copyArtifacts(String carbonHome) throws IOException {
-        // No artifacts need to be copied
-    }
-
-    private static void copyFile(File srcFile, File destFile) throws IOException {
-        if (destFile.exists()) {
-            log.info("File exists in the destination folder and it will be deleted!");
-            destFile.delete();
-        }
-        FileUtils.copyFile(srcFile, destFile);
     }
 
     private String unpackCarbonZip() throws IOException {
@@ -221,8 +163,6 @@ public class ToolboxDeploymentValidationTestCase {
         }
         String carbonHome = serverUtils.setUpCarbonHome(carbonZip);
         TestUtil.copySecurityVerificationService(carbonHome);
-        copyArtifacts(carbonHome);
-
         return carbonHome;
     }
 
@@ -238,9 +178,9 @@ public class ToolboxDeploymentValidationTestCase {
 
     private void initializedRegistryStub(String loggedInSessionCookie,
                                          ConfigurationContext configContext) throws Exception {
-
+        int httpsPort = 9446;
         String EPR = "https://" + FrameworkSettings.HOST_NAME + ":"
-                + 9446//FrameworkSettings.HTTPS_PORT
+                + httpsPort
                 + REGISTRY_SERVICE;
         registryStub = new ResourceAdminServiceStub(configContext, EPR);
         ServiceClient client = registryStub._getServiceClient();
@@ -251,67 +191,40 @@ public class ToolboxDeploymentValidationTestCase {
                 loggedInSessionCookie);
     }
 
-    private void logQueries(String[] queries) {
-        for (int i = 0; i < queries.length; i++) {
-            log.info("query " + i + " : " + queries[i].trim());
-        }
-    }
-
     public String login() throws Exception {
-
         ClientConnectionUtil.waitForPort(Integer.parseInt(FrameworkSettings.HTTPS_PORT) + portOffset);
         AuthenticationAdminStub authAdminStub;
 
         authAdminStub = getAuthAdminStub(null);
 
-        if (log.isDebugEnabled()) {
-            log.debug("UserName : " + FrameworkSettings.USER_NAME + " Password : " +
-                    FrameworkSettings.PASSWORD + " HostName : " + HOST_NAME);
-        }
         boolean isLoggedIn = authAdminStub.login(FrameworkSettings.USER_NAME,
                 FrameworkSettings.PASSWORD, HOST_NAME);
-        assert isLoggedIn : "Login failed!";
-        log.debug("getting sessionCookie");
+        assertTrue(isLoggedIn, "Login failed!");
         ServiceContext serviceContext = authAdminStub.
                 _getServiceClient().getLastOperationContext().getServiceContext();
         sessionCookie = (String) serviceContext.getProperty(HTTPConstants.COOKIE_STRING);
-        assert sessionCookie != null : "Logged in session cookie is null";
-        if (log.isDebugEnabled()) {
-            log.debug("sessionCookie : " + sessionCookie);
-        }
-        log.info("Successfully logged in : " + sessionCookie);
+        assertTrue(sessionCookie != null, "Logged in session cookie is null");
         return sessionCookie;
     }
 
     public void logout() throws Exception {
         AuthenticationAdminStub authenticationAdminStub = getAuthAdminStub(null);
-
-        try {
-            Options options = authenticationAdminStub._getServiceClient().getOptions();
-            options.setProperty(org.apache.axis2.transport.http.HTTPConstants.COOKIE_STRING,
-                    sessionCookie);
-            authenticationAdminStub.logout();
-        } catch (Exception e) {
-            String msg = "Error occurred while logging out";
-            log.error(msg, e);
-            throw new AuthenticationException(msg, e);
-        }
+        Options options = authenticationAdminStub._getServiceClient().getOptions();
+        options.setProperty(org.apache.axis2.transport.http.HTTPConstants.COOKIE_STRING,
+                sessionCookie);
+        authenticationAdminStub.logout();
     }
 
     private AuthenticationAdminStub getAuthAdminStub(String carbonManagementContext) throws AxisFault {
         String authenticationServiceURL;
-        if (carbonManagementContext == null || carbonManagementContext.trim().equals("")) {
+        if (carbonManagementContext == null || carbonManagementContext.trim().isEmpty()) {
             authenticationServiceURL =
-                    "https://localhost:"+ (Integer.parseInt(FrameworkSettings.HTTPS_PORT) + portOffset) +
-                            "/services/AuthenticationAdmin";
+                    "https://" + HOST_NAME + ":" + (Integer.parseInt(FrameworkSettings.HTTPS_PORT) + portOffset) +
+                            AUTHENTICATION_SERVICE;
         } else {
             authenticationServiceURL =
-                    "https://localhost:" + (Integer.parseInt(FrameworkSettings.HTTPS_PORT) + portOffset) +
-                            "/" + carbonManagementContext + "/services/AuthenticationAdmin";
-        }
-
-        if (log.isDebugEnabled()) {
-            log.debug("AuthenticationAdminService URL = " + authenticationServiceURL);
+                    "https://" + HOST_NAME + ":" + (Integer.parseInt(FrameworkSettings.HTTPS_PORT) + portOffset) +
+                            "/" + carbonManagementContext + AUTHENTICATION_SERVICE;
         }
         AuthenticationAdminStub authenticationAdminStub =
                 new AuthenticationAdminStub(authenticationServiceURL);
@@ -320,7 +233,4 @@ public class ToolboxDeploymentValidationTestCase {
         options.setManageSession(true);
         return authenticationAdminStub;
     }
-
-
-
 }
